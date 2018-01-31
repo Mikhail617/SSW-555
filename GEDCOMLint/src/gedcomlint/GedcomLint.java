f6fa00ce7606ff3ece1a268b455d641eaeb8aa63
Mikhail617 <mikhail@rabweb.com>
2018-01-31 18:04:33 -0500

    Move file to root directory. Open file and print contents line by line.

--- GEDCOMLint/src/gedcomlint/GedcomLint.java
+++ GEDCOMLint/src/gedcomlint/GedcomLint.java
@@ -1,6 +1,13 @@
 package gedcomlint;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class GedcomLint {
+	
+	private static File inFile;
 
 	public static void main(String args[]) {
 		// print command line arguments
@@ -10,5 +17,41 @@ public class GedcomLint {
         // open filename from argument
         String filename = args[0];
         System.out.println(filename);
+        
+        if (0 < args.length) {
+            inFile = new File(filename);
+        }
+
+        // print file contents line by line
+        BufferedReader br = null;
+
+        try {
+
+        	String sCurrentLine;
+        	br = new BufferedReader(new FileReader(inFile));
+
+        	while ((sCurrentLine = br.readLine()) != null) {
+        		System.out.println(sCurrentLine);
+        	}
+
+        } 
+
+        catch (IOException e) {
+        	e.printStackTrace();
+        	
+        } 
+
+        finally {
+        	try {
+        		if (br != null)br.close();
+        		
+        	} catch (IOException ex) {
+        		ex.printStackTrace();
+        		
+        	}
+        	
+        }
+        
 	}
+	
 }
