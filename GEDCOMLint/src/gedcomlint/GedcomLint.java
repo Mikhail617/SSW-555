535611d1d7f8d35548922529d6bf0d77a15ca9b8
Poonam Bhilare <poonambhilare@gmail.com>
2018-02-11 16:51:56 -0500

    Completed the total working of Project 02.

--- GEDCOMLint/src/gedcomlint/GedcomLint.java
+++ GEDCOMLint/src/gedcomlint/GedcomLint.java
@@ -3,80 +3,105 @@ package gedcomlint;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import java.io.BufferedReader;
 
 public class GedcomLint {
 	
 	private static File inFile;
+	
+	private static HashMap<String, Set<String>> VALID_LEVEL_TAGS = new HashMap<String, Set<String>>();
+	
+	static {
+		HashSet<String> values = new HashSet<String>();
+		values.add("INDI");
+		values.add("FAM");
+		values.add("HEAD");
+		values.add("TRLR");
+		values.add("NOTE");
+		VALID_LEVEL_TAGS.put("0", values);
+		
+		values = new HashSet<String>();
+		values.add("NAME");
+		values.add("SEX");
+		values.add("BIRT");
+		values.add("DEAT");
+		values.add("FAMC");
+		values.add("FAMS");
+		values.add("MARR");
+		values.add("HUSB");
+		values.add("WIFE");
+		values.add("CHIL");
+		values.add("DIV");
+		VALID_LEVEL_TAGS.put("1", values);
+		
+		values = new HashSet<String>();
+		values.add("DATE");
+		VALID_LEVEL_TAGS.put("2", values);
+	}
 
 	public static void main(String args[]) {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter the name of the file: ");
 		
-		// print command line arguments
-        for (String s: args) {
-            System.out.println(s);
-        }
         // open filename from argument
         String fileName = sc.nextLine();;
-        System.out.println(fileName);
-        
         
         // print file contents line by line
-        
         BufferedReader br = null;
-
         try {
-        	
         	File f = new File(fileName);
-        	InputStream out = new FileInputStream(f);
-
-        	String sCurrentLine;
-        	
-
         	br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
         	
-        	
+        	// read each line from the file
         	for (String line = br.readLine(); line != null; line = br.readLine()) {
-        		System.out.println("--> " + line);  
+        		// Print the input line 
+        		System.out.println("--> " + line);
         		
-        		System.out.print("<--  "); 
-        		
-        		for(int i=0; i<line.length(); i++) {
-        			
-        			String[] sl = line.split(" ");
-        			for (int j = 0 ; j <sl.length ; j++ ) {
-        				System.out.print(sl[j]); 
-        				System.out.print("|");
-        			}
-            		
-        		}
-        		System.out.println(""); 
-
+        		// Print the output line
+        		System.out.print("<-- "); 
+        		// Split the line into words	
+    			String[] words = line.split(" ");
+    			
+    			// check if the line is valid
+    			if(words != null && words.length >= 2) {
+    				
+    				System.out.print(words[0] + "|" + words[1]);
+    				
+    				// check if level and tag is valid
+    				if ( VALID_LEVEL_TAGS.containsKey(words[0]) 
+    						&& VALID_LEVEL_TAGS.get(words[0]).contains(words[1]) ) {
+    					System.out.print("|Y");
+    				} else {
+    					System.out.print("|N");
+    				}
+    				
+    				if(words.length > 2) {
+    					System.out.print("|");
+    					for(int i=2; i<words.length; i++) {
+    						System.out.print(words[i]+ " ");
+    					}
+    				}
+    			}
+    			System.out.println("");
         	}
 
-        } 
-
-        catch (IOException e) {
-        	e.printStackTrace();
-        	
-        } 
-
-        finally {
+        }  catch (IOException e) {
+        	//e.printStackTrace();
+        	System.out.println("Look like you passed incorrect file name");
+        }  finally {
         	try {
         		if (br != null)br.close();
         		
         	} catch (IOException ex) {
         		ex.printStackTrace();
-        		
         	}
-        	
         }
-        
 	}
 	
 }
