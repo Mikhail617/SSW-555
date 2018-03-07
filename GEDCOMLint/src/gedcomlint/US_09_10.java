66ef630272596742c9c579a80d60513e43b09f05
Mikhail617 <mikhail@rabweb.com>
2018-03-07 18:48:00 -0500

    Restoring my changes that got wiped out somehow. Let me know if there are any conflicts.

---
+++ GEDCOMLint/src/gedcomlint/US_09_10.java
@@ -0,0 +1,343 @@
+package gedcomlint;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class US_09_10 {
+	private static List<Individual> allIndividuals = new ArrayList<Individual>();
+	private static List<Family> allFamilies = new ArrayList<Family>();
+
+	public static void parseGedcomFile(File f) throws ParseException {
+        BufferedReader br = null;
+
+        try {
+	    	br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+	    	
+	    	Individual indv = null;
+	    	Family currentFamily = null;
+	    	boolean readingBirthDate = false;
+	    	boolean readingDeathDate = false;
+	    	boolean readingMarriageDate = false;
+	    	boolean readingDivorceDate = false;
+	    	// read each line from the file
+	    	for (String line = br.readLine(); line != null; line = br.readLine()) {
+
+				// Split the line into words	
+				String[] words = line.split(" ");
+
+				// check if the line is valid
+				if(words != null && words.length >= 2) {
+					
+					boolean isSpecialScenario = false;
+					if(words.length >= 3 && (words[2].equals("INDI") || words[2].equals("FAM")) ) {
+						isSpecialScenario = true;
+					}
+					
+					// obtain level, tag and value from the line
+					String level = words[0];
+					String tag = "";
+					String value = "";
+					
+					if(!isSpecialScenario) {
+						tag = words[1];
+						for(int i=2;i<words.length;i++) {
+							value += words[i] + " ";
+						}
+					} else {
+						tag = words[2];
+						value = words[1];
+						for(int i=3;i<words.length;i++) {
+							value += " " +words[i];
+						}
+					}
+					
+					// Check if we encounter INDI record, 
+					if(tag.equals("INDI")) {
+						indv = new Individual();
+						indv.setId(value);
+						
+						allIndividuals.add(indv);
+					}
+					
+					if(tag.equals("NAME") && indv != null) {
+						indv.setName(value);
+					}
+					if(tag.equals("SEX")) {
+						indv.setGender(value);
+					}
+					
+					if(tag.equals("BIRT")) {
+						readingBirthDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingBirthDate) {
+						readingBirthDate = false;
+						indv.setBirthDate(value);
+					}
+					
+					if(tag.equals("MARR")) {
+						readingMarriageDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingMarriageDate) {
+						readingMarriageDate = false;
+						currentFamily.setMarriageDate(value);
+					}
+
+					if(tag.equals("DIV")) {
+						readingDivorceDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingDivorceDate) {
+						readingDivorceDate = false;
+						currentFamily.setDivorceDate(value);
+					}
+
+					if(tag.equals("DEAT")) {
+						readingDeathDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingDeathDate) {
+						readingDeathDate = false;
+						indv.setDeathDate(value);
+					}
+					
+					if(tag.equals("FAM")) {
+						currentFamily = null;
+						for (Family family:allFamilies) {
+							if(family.getId().trim().equals((value.replaceAll("@", "")).trim()))
+								currentFamily = family;
+						}
+						if(currentFamily == null) {
+							currentFamily = new Family();
+							currentFamily.setId(value.replaceAll("@", ""));
+							allFamilies.add(currentFamily);
+						}
+					}
+					
+					if(tag.equals("FAMS")) {
+						indv.getSpouseFamilyIds().add(value);
+						
+						// Let's add the spouse to family table
+						Family fam = null;
+						for (Family family:allFamilies) {
+							if(family.getId().equals(value.replaceAll("@", "")))
+								fam = family;
+						}
+						if(fam == null) {
+							fam = new Family();
+							fam.setId(value.replaceAll("@", ""));
+							allFamilies.add(fam);
+						}
+						if(indv.getGender().trim().equals("M")) {
+							fam.setHusbandName(indv.getName());
+							fam.setHusbandId(indv.getId());
+						} else {
+							fam.setWifeName(indv.getName());
+							fam.setWifeId(indv.getId());
+						}
+					}
+					
+					if(tag.equals("FAMC")) {
+						indv.getChildFamilyIds().add(value);
+						
+						// Let's add the children IDs to the family table
+						Family fam = null;
+						for (Family family:allFamilies) {
+							if(family.getId().equals(value.replaceAll("@", "")))
+								fam = family;
+						}
+						if(fam == null) {
+							fam = new Family();
+							fam.setId(value.replaceAll("@", ""));
+							allFamilies.add(fam);
+						}
+						if(fam.getChildrenId() == null) {
+							List<String> childrenIds = new ArrayList<String>();
+							fam.setChildrenId(childrenIds);
+						} else {
+							if(!fam.getChildrenId().contains(value))
+								fam.getChildrenId().add(value);
+						}
+					}
+					
+				}
+				
+	    	}
+	    	
+	    	checkBirthsBeforeMothersDeaths(allIndividuals, allFamilies);
+	    	checkMarriageBeforeFourteen(allIndividuals, allFamilies);
+	    	
+	    	// at this stage we have all Individuals stored in list named allIndividuals
+	    	System.out.println("Individuals");
+	    	System.out.format("|%1$-10s|%2$-25s|%3$-7s|%4$-12s|%5$-5s|%6$-7s|%7$-12s|%8$-20s|%9$-20s|\n", 
+	    			"----------", "-------------------------", "-------", "------------", "-----", "-------", "------------", "--------------------", "--------------------");
+	    	System.out.format("|%1$-10s|%2$-25s|%3$-7s|%4$-12s|%5$-5s|%6$-7s|%7$-12s|%8$-20s|%9$-20s|\n", 
+	    			"ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse");
+	    	System.out.format("|%1$-10s|%2$-25s|%3$-7s|%4$-12s|%5$-5s|%6$-7s|%7$-12s|%8$-20s|%9$-20s|\n", 
+	    			"----------", "-------------------------", "-------", "------------", "-----", "-------", "------------", "--------------------", "--------------------");
+	    			
+	    	for(int i=0;i<allIndividuals.size();i++) {
+	    		Individual currentIndv = allIndividuals.get(i);
+	    		System.out.format("|%1$-10s|%2$-25s|%3$-7s|%4$-12s|%5$-5s|%6$-7s|%7$-12s|%8$-20s|%9$-20s|\n", 
+	    				currentIndv.getId(), currentIndv.getName(), currentIndv.getGender(),
+	    				currentIndv.getBirthDate(), currentIndv.getAge(), currentIndv.isAlive(),
+	    				currentIndv.getDeathDate(), currentIndv.getChildFamilyIdsAsString(), currentIndv.getSpouseFamilyIdsAsString());
+	    	}
+	    	System.out.format("|%1$-10s|%2$-25s|%3$-7s|%4$-12s|%5$-5s|%6$-7s|%7$-12s|%8$-20s|%9$-20s|\n", 
+	    			"----------", "-------------------------", "-------", "------------", "-----", "-------", "------------", "--------------------", "--------------------");
+	    	
+	    	// Now, let's print all families stored in list named allFamilies
+	    	System.out.println("Families");
+	    	System.out.format("|%1$-10s|%2$-12s|%3$-12s|%4$-5s|%5$-25s|%6$-10s|%7$-25s|%8$-20s|\n", 
+	    			"----------", "------------", "------------", "----------", "-------------------------", "----------", "-------------------------", "--------------------");
+	    	
+	    	
+	    	System.out.format("|%1$-10s|%2$-12s|%3$-12s|%4$-5s|%5$-25s|%6$-10s|%7$-25s|%8$-20s|\n", 
+	    			"ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children");
+	    	System.out.format("|%1$-10s|%2$-12s|%3$-12s|%4$-5s|%5$-25s|%6$-10s|%7$-25s|%8$-20s|\n", 
+	    			"----------", "------------", "------------", "----------", "-------------------------", "----------", "-------------------------", "--------------------");
+	    	for(Family fam: allFamilies)
+	    		System.out.format("|%1$-10s|%2$-12s|%3$-12s|%4$-10s|%5$-25s|%6$-10s|%7$-25s|%8$-20s|\n", 
+	    				fam.getId(), fam.getMarriageDate() == null ? "NA" : fam.getMarriageDate(), fam.getDivorceDate() == null ? "NA" : fam.getDivorceDate(), fam.getHusbandId(),
+	    								fam.getHusbandName(),fam.getWifeId(), fam.getWifeName(),fam.getChildrenIdAsString());
+	    	System.out.format("|%1$-10s|%2$-12s|%3$-12s|%4$-10s|%5$-25s|%6$-10s|%7$-25s|%8$-20s|\n", 
+	    			"----------", "------------", "------------", "----------", "-------------------------", "----------", "-------------------------", "--------------------");
+	    							
+        
+        }  catch (IOException e) {
+        	//e.printStackTrace();
+        	System.out.println("Look like you passed incorrect file name");
+        }  finally {
+        	try {
+        		if (br != null)br.close();
+        		
+        	} catch (IOException ex) {
+        		ex.printStackTrace();
+        	}
+        }
+
+	}
+	
+	// Check all of the children't birthdates to make sure that
+	// it's before the mother's death and, if the father is dead,
+	// make sure that the birth is less than nine months before the
+	// father's death date.
+	public static String[] checkBirthsBeforeMothersDeaths(List<Individual> allIndividuals, List<Family> allFamilies) throws ParseException {
+		//System.out.println("Start: Validating birthdates are before mother's death date.");
+		String[] errors = new String[allIndividuals.size()];
+		int error_index = 0;
+		for(Individual ind: allIndividuals)	 {
+			Individual mother = null;
+			Individual father = null;
+			String motherId = null;
+			String fatherId = null;
+			Date birthdate;
+			SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
+			if(!ind.getChildFamilyIds().isEmpty()) {
+				String famId = ind.getChildFamilyIdsAsString().replaceAll("\\{", "")
+						.replaceAll("\\}", "").replaceAll("'", "");
+				birthdate = sdf.parse(ind.getBirthDate());
+				for(Family fam: allFamilies) {
+					if(fam.getId().equals(famId)) {
+						motherId = fam.getWifeId();
+						fatherId = fam.getHusbandId();
+						break;
+					}
+				}
+				for(Individual i: allIndividuals) {
+					if(i.getId().equals(motherId)) {
+						if(i.isAlive().equals("True")) {
+							//System.out.println("Mother of child " + famId + " is still alive.");
+						} else {
+							Date deathdate = sdf.parse(i.getDeathDate());
+							if(deathdate.before(birthdate)) {
+								String error = "ERROR: INDIVIDUAL: " + i.getId() + ": Mother's death date cannot be before child's birthdate.";
+								System.out.println(error);
+								errors[error_index++] = error;
+								
+							} else {
+								//System.out.println("Mother's death is after childs birth.");
+							}
+						}
+					}
+					if(i.getId().equals(fatherId)) {
+						if(i.isAlive().equals("True")) {
+							//System.out.println("Father of child " + famId + " is still alive.");
+						} else {
+							Calendar deathdate = Calendar.getInstance();
+							//Calendar deathdate_plus_nine_months = Calendar.getInstance();
+							String[] death_date_params = i.getDeathDate().split(" ");
+							Date month = new SimpleDateFormat("MMM").parse(death_date_params[1]);
+							deathdate.set(Integer.parseInt(death_date_params[2]), month.getMonth(), 
+									Integer.parseInt(death_date_params[0]));
+							//deathdate_plus_nine_months.set(Integer.parseInt(death_date_params[2]), month.getMonth(), 
+							//		Integer.parseInt(death_date_params[0]));
+							//deathdate_plus_nine_months.add(GregorianCalendar.MONTH, 9);
+							deathdate.add(GregorianCalendar.MONTH, 9);
+							if(birthdate.after(deathdate.getTime())) {
+								String warning = "ERROR: INDIVIDUAL: " + i.getId() + ": Father's death date cannot be more than 9 months before child's birthdate.";
+								System.out.println(warning);
+								errors[error_index++] = warning;
+							}
+							//else if(birthdate.after(deathdate_plus_nine_months.getTime())) {
+							//	String error = "ERROR: Father's death date cannot be more than 9 months before child's birthdate.";
+							//	System.out.println(error);
+							//	errors[error_index++] = error;								
+							//}
+						}
+					}
+				}
+			}
+		}
+		if (error_index == 0) {
+			errors[error_index++] = "No errors found."; 
+		}
+		//System.out.println("Complete: Validating birthdates are before mother's death date.");
+		return errors;
+	}
+	
+	public static String[] checkMarriageBeforeFourteen(List<Individual> allIndividuals, List<Family> allFamilies) throws ParseException {
+		//System.out.println("Start: Validating that marriage dates are after the individual is fourteen years of age.");
+		String[] errors = new String[allIndividuals.size()];
+		int error_index = 0;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
+		for(Family family: allFamilies) {
+			String husbandId = family.getHusbandId();
+			String wifeId = family.getWifeId();
+			Date marriageDate = sdf.parse(family.getMarriageDate());
+			for(Individual individual: allIndividuals) {
+				if(individual.getId().equals(husbandId) || individual.getId().equals(wifeId)) {
+					Calendar birthdate = Calendar.getInstance();
+					String[] birthdate_params = individual.getBirthDate().split(" ");
+					Date month = new SimpleDateFormat("MMM").parse(birthdate_params[1]);
+					birthdate.set(Integer.parseInt(birthdate_params[2]), month.getMonth(), 
+							Integer.parseInt(birthdate_params[0]));
+					birthdate.add(GregorianCalendar.YEAR, 14);
+					if(marriageDate.before(birthdate.getTime())) {
+						String error = "ERROR: INDIVIDUAL: US10: " + individual.getId() + ": Marriage date cannot be less than fourteen years after person's birthdate: " + individual.getBirthDate();
+						System.out.println(error);
+						errors[error_index++] = error;
+					}
+				}
+			}
+		}
+		
+		if (error_index == 0) {
+			errors[error_index++] = "No errors found."; 
+		}
+		//System.out.println("Complete: Validating that marriage dates are after the individual is fourteen years of age.");
+		return errors;
+	}
+}
---
+++ GEDCOMLint/src/unittests/UnitTests.java
@@ -0,0 +1,260 @@
+package unittests;
+
+import org.junit.Test;
+
+import gedcomlint.Family;
+import gedcomlint.Individual;
+import gedcomlint.US_09_10;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitTests {
+	
+	private static List<Individual> allIndividuals = new ArrayList<Individual>();
+	private static List<Family> allFamilies = new ArrayList<Family>();
+	
+	   @Test
+	   public void testSetup() {
+	        BufferedReader br = null;
+
+	        try {
+	        	
+	        	File f = new File("unit_test.ged");
+		    	br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+		    	
+		    	Individual indv = null;
+		    	Family currentFamily = null;
+		    	boolean readingBirthDate = false;
+		    	boolean readingDeathDate = false;
+		    	boolean readingMarriageDate = false;
+		    	boolean readingDivorceDate = false;
+		    	// read each line from the file
+		    	for (String line = br.readLine(); line != null; line = br.readLine()) {
+
+					// Split the line into words	
+					String[] words = line.split(" ");
+
+					// check if the line is valid
+					if(words != null && words.length >= 2) {
+						
+						boolean isSpecialScenario = false;
+						if(words.length >= 3 && (words[2].equals("INDI") || words[2].equals("FAM")) ) {
+							isSpecialScenario = true;
+						}
+						
+						// obtain level, tag and value from the line
+						String level = words[0];
+						String tag = "";
+						String value = "";
+						
+						if(!isSpecialScenario) {
+							tag = words[1];
+							for(int i=2;i<words.length;i++) {
+								value += words[i] + " ";
+							}
+						} else {
+							tag = words[2];
+							value = words[1];
+							for(int i=3;i<words.length;i++) {
+								value += " " +words[i];
+							}
+						}
+						
+						// Check if we encounter INDI record, 
+						if(tag.equals("INDI")) {
+							indv = new Individual();
+							indv.setId(value);
+							
+							allIndividuals.add(indv);
+						}
+						
+						if(tag.equals("NAME") && indv != null) {
+							indv.setName(value);
+						}
+						if(tag.equals("SEX")) {
+							indv.setGender(value);
+						}
+						
+						if(tag.equals("BIRT")) {
+							readingBirthDate = true;
+							continue;
+						}
+						if(tag.equals("DATE") && readingBirthDate) {
+							readingBirthDate = false;
+							indv.setBirthDate(value);
+						}
+						
+						if(tag.equals("MARR")) {
+							readingMarriageDate = true;
+							continue;
+						}
+						if(tag.equals("DATE") && readingMarriageDate) {
+							readingMarriageDate = false;
+							currentFamily.setMarriageDate(value);
+						}
+
+						if(tag.equals("DIV")) {
+							readingDivorceDate = true;
+							continue;
+						}
+						if(tag.equals("DATE") && readingDivorceDate) {
+							readingDivorceDate = false;
+							currentFamily.setDivorceDate(value);
+						}
+
+						if(tag.equals("DEAT")) {
+							readingDeathDate = true;
+							continue;
+						}
+						if(tag.equals("DATE") && readingDeathDate) {
+							readingDeathDate = false;
+							indv.setDeathDate(value);
+						}
+						
+						if(tag.equals("FAM")) {
+							currentFamily = null;
+							for (Family family:allFamilies) {
+								if(family.getId().trim().equals((value.replaceAll("@", "")).trim()))
+									currentFamily = family;
+							}
+							if(currentFamily == null) {
+								currentFamily = new Family();
+								currentFamily.setId(value.replaceAll("@", ""));
+								allFamilies.add(currentFamily);
+							}
+						}
+						
+						if(tag.equals("FAMS")) {
+							indv.getSpouseFamilyIds().add(value);
+							
+							// Let's add the spouse to family table
+							Family fam = null;
+							for (Family family:allFamilies) {
+								if(family.getId().equals(value.replaceAll("@", "")))
+									fam = family;
+							}
+							if(fam == null) {
+								fam = new Family();
+								fam.setId(value.replaceAll("@", ""));
+								allFamilies.add(fam);
+							}
+							if(indv.getGender().trim().equals("M")) {
+								fam.setHusbandName(indv.getName());
+								fam.setHusbandId(indv.getId());
+							} else {
+								fam.setWifeName(indv.getName());
+								fam.setWifeId(indv.getId());
+							}
+						}
+						
+						if(tag.equals("FAMC")) {
+							indv.getChildFamilyIds().add(value);
+							
+							// Let's add the children IDs to the family table
+							Family fam = null;
+							for (Family family:allFamilies) {
+								if(family.getId().equals(value.replaceAll("@", "")))
+									fam = family;
+							}
+							if(fam == null) {
+								fam = new Family();
+								fam.setId(value.replaceAll("@", ""));
+								allFamilies.add(fam);
+							}
+							if(fam.getChildrenId() == null) {
+								List<String> childrenIds = new ArrayList<String>();
+								fam.setChildrenId(childrenIds);
+							} else {
+								if(!fam.getChildrenId().contains(value))
+									fam.getChildrenId().add(value);
+							}
+						}
+						
+					}
+					
+		    	}
+	        }  catch (IOException e) {
+	        	//e.printStackTrace();
+	        	System.out.println("Look like you passed incorrect file name");
+	        }  finally {
+	        	try {
+	        		if (br != null)br.close();
+	        		
+	        	} catch (IOException ex) {
+	        		ex.printStackTrace();
+	        	}
+	        }
+	   }
+	   
+	   @Test
+	   public void testBirthAfterMotherDeath() {
+		   String msg = "ERROR: INDIVIDUAL: I3: Mother's death date cannot be before child's birthdate.";
+		   String[] result = null;
+		   try {
+			result = US_09_10.checkBirthsBeforeMothersDeaths(allIndividuals, allFamilies);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   assertEquals(msg, result[0]);
+	   }
+	   
+	   @Test
+	   public void testBirthMoreThanNineMonthsAfterFatherDeath() {
+		   String msg = "ERROR: INDIVIDUAL: I9: Father's death date cannot be more than 9 months before child's birthdate.";
+		   String[] result = null;
+		   try {
+			result = US_09_10.checkBirthsBeforeMothersDeaths(allIndividuals, allFamilies);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   assertEquals(msg, result[1]);		   
+	   }
+	   
+	   /*@Test
+	   public void testBirthLessThanNineMonthsAfterFatherDeath() {
+		   String msg = "WARNING: Child born after father's death";
+		   String[] result = null;
+		   try {
+			result = US09.checkBirthsBeforeMothersDeaths(allIndividuals, allFamilies);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   assertEquals(msg, result[2]);			   
+	   }*/
+	   
+	   
+	   @Test
+	   public void testMarriageBeforeFourteenYearsOldWife() {
+		   String msg = "ERROR: INDIVIDUAL: US10: I1: Marriage date cannot be less than fourteen years after person's birthdate: 10 JUL 2009 ";
+		   String[] result = null;
+		   try {
+			result = US_09_10.checkMarriageBeforeFourteen(allIndividuals, allFamilies);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		   assertEquals(msg, result[0]);
+	   }
+	   
+	   @Test 
+	   public void testMarriageBeforeFourteenYearsOldHusband() {
+		   
+	   }
+	   
+	   @Test
+	   public void testNormalCase() {
+		   
+	   }
+}
