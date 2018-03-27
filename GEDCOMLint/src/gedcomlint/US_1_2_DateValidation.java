365b94acba58dcd4554aa713ac5d7c7a870cbbf2
Poonam Bhilare <poonambhilare@gmail.com>
2018-03-26 22:21:53 -0400

    Updating US 1 and 2 based on refactored code

--- GEDCOMLint/src/gedcomlint/US_1_2_DateValidation.java
+++ GEDCOMLint/src/gedcomlint/US_1_2_DateValidation.java
@@ -1,175 +1,14 @@
 package gedcomlint;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class US_1_2_DateValidation {
-	private List<Individual> allIndividuals = new ArrayList<Individual>();
-	private List<Family> allFamilies = new ArrayList<Family>();
-
-	public void readIndividualsAndFamilies(File f) {
-        BufferedReader br = null;
-
-        try {
-	    	br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
-	    	
-	    	Individual indv = null;
-	    	Family currentFamily = null;
-	    	boolean readingBirthDate = false;
-	    	boolean readingDeathDate = false;
-	    	boolean readingMarriageDate = false;
-	    	boolean readingDivorceDate = false;
-	    	// read each line from the file
-	    	for (String line = br.readLine(); line != null; line = br.readLine()) {
-
-				// Split the line into words	
-				String[] words = getWordsFromLine(line);
-
-				// check if the line is valid
-				if(isValidLine(words)) {
-					
-					// obtain level, tag and value from the line
-					String level = getLevel(words);
-					String tag = getTag(words);
-					String value = getValue(words);
-					
-					// Check if we encounter INDI record, 
-					if(tag.equals("INDI")) {
-						indv = new Individual();
-						indv.setId(value);
-						
-						allIndividuals.add(indv);
-					}
-					
-					if(tag.equals("NAME") && indv != null) {
-						indv.setName(value);
-					}
-					if(tag.equals("SEX")) {
-						indv.setGender(value);
-					}
-					
-					if(tag.equals("BIRT")) {
-						readingBirthDate = true;
-						continue;
-					}
-					if(tag.equals("DATE") && readingBirthDate) {
-						readingBirthDate = false;
-						indv.setBirthDate(value);
-					}
-					
-					if(tag.equals("MARR")) {
-						readingMarriageDate = true;
-						continue;
-					}
-					if(tag.equals("DATE") && readingMarriageDate) {
-						readingMarriageDate = false;
-						currentFamily.setMarriageDate(value);
-					}
-
-					if(tag.equals("DIV")) {
-						readingDivorceDate = true;
-						continue;
-					}
-					if(tag.equals("DATE") && readingDivorceDate) {
-						readingDivorceDate = false;
-						currentFamily.setDivorceDate(value);
-					}
-
-					if(tag.equals("DEAT")) {
-						readingDeathDate = true;
-						continue;
-					}
-					if(tag.equals("DATE") && readingDeathDate) {
-						readingDeathDate = false;
-						indv.setDeathDate(value);
-					}
-					
-					if(tag.equals("FAM")) {
-						currentFamily = null;
-						for (Family family:allFamilies) {
-							if(family.getId().trim().equals((value.replaceAll("@", "")).trim()))
-								currentFamily = family;
-						}
-						if(currentFamily == null) {
-							currentFamily = new Family();
-							currentFamily.setId(value.replaceAll("@", ""));
-							allFamilies.add(currentFamily);
-						}
-					}
-					
-					if(tag.equals("FAMS")) {
-						indv.getSpouseFamilyIds().add(value);
-						
-						// Let's add the spouse to family table
-						Family fam = null;
-						for (Family family:allFamilies) {
-							if(family.getId().equals(value.replaceAll("@", "")))
-								fam = family;
-						}
-						if(fam == null) {
-							fam = new Family();
-							fam.setId(value.replaceAll("@", ""));
-							allFamilies.add(fam);
-						}
-						if(indv.getGender().trim().equals("M")) {
-							fam.setHusbandName(indv.getName());
-							fam.setHusbandId(indv.getId());
-						} else {
-							fam.setWifeName(indv.getName());
-							fam.setWifeId(indv.getId());
-						}
-					}
-					
-					if(tag.equals("FAMC")) {
-						indv.getChildFamilyIds().add(value);
-						
-						// Let's add the children IDs to the family table
-						Family fam = null;
-						for (Family family:allFamilies) {
-							if(family.getId().equals(value.replaceAll("@", "")))
-								fam = family;
-						}
-						if(fam == null) {
-							fam = new Family();
-							fam.setId(value.replaceAll("@", ""));
-							allFamilies.add(fam);
-						}
-						if(fam.getChildrenId() == null) {
-							List<String> childrenIds = new ArrayList<String>();
-							fam.setChildrenId(childrenIds);
-						} else {
-							if(!fam.getChildrenId().contains(value))
-								fam.getChildrenId().add(value);
-						}
-					}
-					
-				}
-	    	}
-	    	
-        }  catch (IOException e) {
-        	//e.printStackTrace();
-        	System.out.println("Look like you passed incorrect file name");
-        }  finally {
-        	try {
-        		if (br != null)br.close();
-        		
-        	} catch (IOException ex) {
-        		ex.printStackTrace();
-        	}
-        }
-
-	}
 	
-	public void validateDateGreaterThanToday() throws ParseException {
+	public void validateDateGreaterThanToday(List<Individual> allIndividuals, List<Family> allFamilies) throws ParseException {
 		
     	// at this stage we have all Individuals and Families 
     	for(Individual currentIndv : allIndividuals) {
@@ -189,11 +28,11 @@ public class US_1_2_DateValidation {
     	}
 	}
 
-	public void validateBirthBeforeMarriage() throws ParseException {
+	public void validateBirthBeforeMarriage(List<Individual> allIndividuals, List<Family> allFamilies) throws ParseException {
     	// at this stage we have all Individuals and Families 
     	for(Individual currentIndv : allIndividuals) {
     		
-    		Family famOfPerson = getFamily(currentIndv.getId());
+    		Family famOfPerson = getFamily(allFamilies, currentIndv.getId());
     		if(famOfPerson != null) {
         		if(isGreater(currentIndv.getBirthDate(), famOfPerson.getMarriageDate())) {
         			System.out.println("ERROR: INDIVIDUAL: US02: " +currentIndv.id + ": Birthdate " + currentIndv.birthDate + " occurs after marriage date " + famOfPerson.getMarriageDate()+".");
@@ -202,7 +41,7 @@ public class US_1_2_DateValidation {
     	}
 	}
 
-	private Family getFamily(String id) {
+	private Family getFamily(List<Family> allFamilies, String id) {
     	for(Family fam: allFamilies) {
     		if( fam.getHusbandId() != null && fam.getHusbandId().equals(id) ) {
     			return fam;
